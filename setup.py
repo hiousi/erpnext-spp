44eb2064bb476a2460a0f34085d61537bd60b2f3
hiousi <o10c@yahoo.fr>
2018-10-20 10:10:25 +0200

    update requirements setup

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in spp/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('spp/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='spp',
 	version=version,
 	description='SPP makes stock entries from BOMs',
-	author='o10c',
-	author_email='o10c@yahoo.fr',
+	author='hiousi',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
+
 )
